717fc5a012016a2626b152d2d1a6f3477db529c3
ltsimps <ltsimpso@gmail.com>
2024-02-24 21:24:58 -0500

    Corrrected Form error

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -2,20 +2,34 @@ import React from 'react';
 
 function Guess() {
    const [word, setWord] = React.useState('');
+   const [guesses, setGuesses] = React.useState([]);
+
 
 
   return(
     <div> 
     <h2>Guess the word:</h2>
-   <form className="guess-input-wrapper" onSubmit={()=> {
-    event.preventDefault();
-    console.log(word);
-    setWord('');
 
-   }}>
+  
+    
+    <form className="guess-input-wrapper" onSubmit={(event) => {
+  event.preventDefault();
+  console.log(word);
+  setGuesses([...guesses, word]);
+  console.log(guesses);
+  setWord('');
+}}>
   <label htmlFor="guess-input">Enter guess:</label>
-  <input id="guess-input" type="text" value={word} onChange={setWord(event.target.value)} />
+  <input id="guess-input" type="text" value={word} onChange={(event) => setWord(event.target.value)} />
 </form>
+
+
+<ul>
+  {guesses.map((guess, index) => {
+    return <li key={index}>{guess}</li>
+  }
+  )}  
+</ul>
     </div>)
 }
 
